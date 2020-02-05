3ca3efeb2368487472ab99fe416cdb66cc0331ac
Aubin Detrez <aubin.detrez@ibm.com>
2020-02-05 14:17:25 +0100

    WIP add tire resistance

--- random/bikecalculator.py
+++ random/bikecalculator.py
@@ -1,3 +1,11 @@
 # This script is based on http://bikecalculator.com/
 # cf http://bikecalculator.com/bikecalculator.js
 
+class Tire:
+    tireResistance = [0.005, 0.004, 0.012]
+    def __init__(self, tireType=0):
+        self.resistance = self.tireResistance[tireType]
+    def getResistance(self):
+        return self.resistance
+
+print(Tire(1).getResistance())
