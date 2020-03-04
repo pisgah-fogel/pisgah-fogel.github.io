bf82e742d401ea6254b5c679e2a8a4174d3184a0
Aubin Detrez <aubin.detrez@ibm.com>
2020-03-04 15:53:42 +0100

    WIP write HTLM page from dic

--- random/pycyclinggraph/main.py
+++ random/pycyclinggraph/main.py
@@ -39,10 +39,54 @@ def tsv_to_dic(filename):
     print("Error: Cannot open file %s" % filename)
     return None
 
+def dic_to_html(filename, dic):
+    with open(filename,"w") as file:
+        file.write("<!DOCTYPE html>")
+        file.write("<html lang=\"en\">")
+        file.write("<head>")
+        file.write("<meta charset=\"UTF-8\">")
+
+        # CSS Style
+        file.write("\
+        <style type=\"text/css\"> \
+        table, td, th { \
+            border: 1px solid gray; \
+        } \
+        </style>\
+        ")
+
+        file.write("</head>")
+        file.write("<body>")
+        table = "<table><caption>"
+        table += "Training logs"
+        table += "</caption><thead>"
+
+        table += "<tr>"
+        table += "<th>Weather</th>"
+        table += "<th>Date</th>"
+        table += "<th>Training</th>"
+        table += "</tr>"
+
+        table += "</thead><tbody>"
+
+        for row in dic:
+            table += "<tr>"
+            table += "<td>%s</td>" % row["Weather/BB"]
+            table += "<td>%s</td>" % row["Date"]
+            table += "<td>%s</td>" % row["Training"]
+            table += "</tr>"
+        table += "</tbody></table>"
+
+        file.write(table)
+        file.write("</body>")
+        file.write("</html>")
+
 if __name__== "__main__":
     filename = "C:\\Users\\AubinDetrez\\Documents\\Personnal\\pisgah-fogel.github.io\\random\\pycyclinggraph\\Trainings - Year 2020.tsv"
     if not check_num_col(filename, 19):
         print("Error: IO error or malformed file ")
         exit(1)
     
-    print(tsv_to_dic(filename))
+    dic = tsv_to_dic(filename)
+
+    dic_to_html("index.html",dic)
