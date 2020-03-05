206aabddab2d9ef7625572969dd1bc014955ee29
Aubin Detrez <aubin.detrez@ibm.com>
2020-03-05 12:41:55 +0100

    Create tab (with javascript)

--- random/pycyclinggraph/main.py
+++ random/pycyclinggraph/main.py
@@ -92,12 +92,73 @@ def dic_to_html(filename, dic):
         table, td, th { \
             border: 1px solid gray; \
         } \
+        .tab {\
+        overflow: hidden;\
+        border: 1px solid #ccc;\
+        background-color: #f1f1f1;\
+        }\
+        .tab button {\
+        background-color: inherit;\
+        float: left;\
+        border: none;\
+        outline: none;\
+        cursor: pointer;\
+        padding: 14px 16px;\
+        transition: 0.3s;\
+        }\
+        .tab button:hover {\
+        background-color: #ddd;\
+        }\
+        .tab button.active {\
+        background-color: #ccc;\
+        }\
+        .tabcontent {\
+        display: none;\
+        padding: 6px 12px;\
+        border: 1px solid #ccc;\
+        border-top: none;\
+        }\
         </style>\
+        ")
+
+        file.write("\
+        <script>\
+        function openTab(evt, TabName) {\
+            var i, tabcontent, tablinks;\
+            tabcontent = document.getElementsByClassName(\"tabcontent\");\
+            for (i = 0; i < tabcontent.length; i++) {\
+                tabcontent[i].style.display = \"none\";\
+            }\
+            tablinks = document.getElementsByClassName(\"tablinks\");\
+            for (i = 0; i < tablinks.length; i++) {\
+                tablinks[i].className = tablinks[i].className.replace(\" active\", \"\");\
+            }\
+            document.getElementById(TabName).style.display = \"block\";\
+            evt.currentTarget.className += \" active\";\
+        }\
+        </script>\
         ")
 
         file.write("</head>")
         file.write("<body>")
+        file.write("<div class=\"tab\">\
+            <button class=\"tablinks\" onclick=\"openTab(event, 'Logs')\">Logs</button>\
+            <button class=\"tablinks\" onclick=\"openTab(event, 'Weeks')\">Weeks</button>\
+            <button class=\"tablinks\" onclick=\"openTab(event, 'Graphs')\">Graphs</button>\
+            </div>")
+
+        file.write("<div id=\"Logs\" class=\"tabcontent\">")
         file.write(dic_to_table(dic))
+        file.write("</div>")
+
+        file.write("<div id=\"Weeks\" class=\"tabcontent\">")
+        # TODO
+        file.write("</div>")
+
+        file.write("<div id=\"Graphs\" class=\"tabcontent\">")
+        # TODO
+        file.write("</div>")
+
         file.write("</body>")
         file.write("</html>")
 
