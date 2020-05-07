eaf77486f7047fac6c8059574eebf429f23e7485
Phileas Fogg <phileas.fogg@mail.com>
2020-05-07 17:42:20 +0200

    Create terrain

--- random/3d_map_irrlicht/main.cpp
+++ random/3d_map_irrlicht/main.cpp
@@ -41,8 +41,64 @@ int main()
 		node->setMaterialTexture( 0, driver->getTexture("media/sydney.bmp") );
 	}
 	//smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
-	smgr->addCameraSceneNodeFPS();
+	scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeFPS();
+
+	camera->setPosition(core::vector3df(2700*2,255*2,2600*2));
+    camera->setTarget(core::vector3df(2397*2,343*2,2700*2));
+    camera->setFarValue(42000.0f);
+
 	device->getCursorControl()->setVisible(false);
+
+    scene::ITerrainSceneNode* terrain = smgr->addTerrainSceneNode(
+        "media/terrain-heightmap.bmp",
+        0,                  // parent node
+        -1,                 // node id
+        core::vector3df(0.f, 0.f, 0.f),     // position
+        core::vector3df(0.f, 0.f, 0.f),     // rotation
+        core::vector3df(40.f, 4.4f, 40.f),  // scale
+        video::SColor ( 255, 255, 255, 255 ),   // vertexColor
+        5,                  // maxLOD
+        scene::ETPS_17,             // patchSize
+        4                   // smoothFactor
+        );
+
+    terrain->setMaterialFlag(video::EMF_LIGHTING, false);
+
+    terrain->setMaterialTexture(0,
+            driver->getTexture("media/terrain-texture.jpg"));
+    terrain->setMaterialTexture(1,
+            driver->getTexture("media/detailmap3.jpg"));
+    
+    terrain->setMaterialType(video::EMT_DETAIL_MAP);
+
+    terrain->scaleTexture(1.0f, 20.0f);
+
+	// create triangle selector for the terrain 
+    scene::ITriangleSelector* selector
+        = smgr->createTerrainTriangleSelector(terrain, 0);
+    terrain->setTriangleSelector(selector);
+
+    // create collision response animator and attach it to the camera
+    scene::ISceneNodeAnimator* anim = smgr->createCollisionResponseAnimator(
+        selector, camera, core::vector3df(60,100,60),
+        core::vector3df(0,0,0),
+        core::vector3df(0,50,0));
+    selector->drop();
+    camera->addAnimator(anim);
+    anim->drop();
+
+	driver->setTextureCreationFlag(video::ETCF_CREATE_MIP_MAPS, false);
+
+    scene::ISceneNode* skybox=smgr->addSkyBoxSceneNode(
+        driver->getTexture("media/irrlicht2_up.jpg"),
+        driver->getTexture("media/irrlicht2_dn.jpg"),
+        driver->getTexture("media/irrlicht2_lf.jpg"),
+        driver->getTexture("media/irrlicht2_rt.jpg"),
+        driver->getTexture("media/irrlicht2_ft.jpg"),
+        driver->getTexture("media/irrlicht2_bk.jpg"));
+    scene::ISceneNode* skydome=smgr->addSkyDomeSceneNode(driver->getTexture("media/skydome.jpg"),16,8,0.95f,2.0f);
+
+    driver->setTextureCreationFlag(video::ETCF_CREATE_MIP_MAPS, true);
 
 	while(device->run())
 	{
